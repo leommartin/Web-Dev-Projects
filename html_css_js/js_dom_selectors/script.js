a6246afb36ca7943bc069482f64b6375db935540
Leonardo Martin <leowork2002@gmail.com>
2025-07-09 10:13:43 -0400

    docs: add comments to script

--- html_css_js/js_dom_selectors/script.js
+++ html_css_js/js_dom_selectors/script.js
@@ -1,8 +1,21 @@
 // Exibir no console título, primeiro parágrafo, 
 // e todos os parágrafos com '.descricao'
 
+// In this script, we try to learn how to
+// catch an HTML object (by tag,  by id, by class)
+// and change style, content, visibility
+// add and remove classes using
+// getElementById, querySelector, querySelectorAll
+// addEventListener("click") and classList toggle
+// which is a variation of classList.add n .remove
+
+
 let titulo = document.getElementById("titulo");
 console.log(titulo.textContent);
+
+// We can apply HTML 
+// titulo.innerHTML = "<span style='color: blue;'>Título com HTML</span>";
+
 // We can apply style's
 // titulo.style.color = "red";
 // titulo.style.backgroundColor = "black";
@@ -29,6 +42,7 @@ descrs.forEach(item => console.log(item.textContent));
 let botao = document.getElementById("botaoEstilo");
 
 botao.addEventListener("click", () => {
+
     titulo.classList.toggle("destaque");
 
     if(titulo.classList.contains("destaque"))
@@ -45,4 +59,7 @@ let btn_mudar_cor = document.getElementById("botaoMudarCor");
 
 btn_mudar_cor.addEventListener("click", () => {
     btn_mudar_cor.classList.toggle("mudacor");
+
+    // The h1 tag still exists, but now with this new content inside
+    titulo.innerHTML = "Novo <em>texto</em> com <strong>HTML</strong>";
 })
