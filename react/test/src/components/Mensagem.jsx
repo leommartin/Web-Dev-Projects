d8b7cdd58d42a7cfdbeb2a92fd0fe62b32786cd6
Leonardo Martin <leowork2002@gmail.com>
2025-07-15 15:29:22 -0400

    docs: add comments to Counter Component

--- react/test/src/components/Mensagem.jsx
+++ react/test/src/components/Mensagem.jsx
@@ -1,4 +1,7 @@
 function Mensagem(props){
+
+    // Here, props is an object that contains the properties passed to the component.
+    // We also can use {nome, idade} instead of props if we want to destructure the props.
     return (
         <p>Olá {props.nome}! Você tem {props.idade} anos.</p>
     );
