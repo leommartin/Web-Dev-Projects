bcafeeacd7cc84f81802b8ea126be2be0c26b4a5
Leonardo Martin <leowork2002@gmail.com>
2025-07-23 09:41:46 -0400

    feat: add title with to-do-list component

--- react/to-do-list/src/App.jsx
+++ react/to-do-list/src/App.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ToDoList from './components/ToDoList.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    
+    <div style={{padding: '2rem', fontFamily: 'Arial, sans-serif'}}>
+      <h1>Minhas tarefas</h1>
+      <ToDoList />
+    
+    </div>
 
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
