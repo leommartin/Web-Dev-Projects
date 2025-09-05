602b5c0804dbc6843b847e7f08a746f541f3afc4
Leonardo Martin <leowork2002@gmail.com>
2025-09-05 17:08:39 -0300

    chore: App.js - using useState

--- react/curso-react/primeiro-app/src/App.js
+++ react/curso-react/primeiro-app/src/App.js
@@ -1,20 +1,23 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import { useState } from 'react';
 import Nome from './components/Nome';
 
 function App() {
+
+  const [aluno, setAluno] = useState("Leonardo");
+
+  function handleChangeName(nome){
+    setAluno(nome);
+  }
   return (
     <div className="App">
-      <h1> Bem vindo ao meu projeto!</h1>
-      <Nome aluno="Lucas" idade="22"/>
-      <br/>
-      <Nome aluno="Jose Silva" idade="30"/>
+      <h1> Component App</h1>
+      <h2> Olá: {aluno} </h2>
+      <button onClick={() => handleChangeName('Lucas Oliveira')}>Mudar nome</button>
     </div>
   );
 }
-
-
-
 
 export default App;
