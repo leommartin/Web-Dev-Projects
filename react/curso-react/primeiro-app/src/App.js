c2c76f4a4bb45047ff9cc89259341132383a8754
Leonardo Martin <leowork2002@gmail.com>
2025-09-06 11:19:34 -0300

    chore: other example with useState: task list

--- react/curso-react/primeiro-app/src/App.js
+++ react/curso-react/primeiro-app/src/App.js
@@ -2,55 +2,35 @@
 // import './App.css';
 
 import { useState } from 'react';
-import Nome from './components/Nome';
 
 function App() {
 
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState(0);
+  const [tarefas, setTarefas] = useState([
+    'Pagar conta de luz',
+    'Estudar React'
+  ]);
+  const [input, setInput] = useState('');
 
-  const [user, setUser] = useState({});
 
   function handleRegister(e){
     e.preventDefault();
-    
-    alert("Usuário cadastrado com Sucesso!");
-    setUser({
-      nome: nome, 
-      email: email,
-      idade: idade
-    })
 
+    setTarefas([...tarefas, input]);
+    setInput('');
   }
 
   return (
     <div>
 
-      <h1>Cadastrar Usuário</h1>
+      <h1>Cadastrar Tarefa</h1>
+
       <form onSubmit={handleRegister}>
-        <label>Nome</label><br/>
+        <label>Nome da tarefa:</label><br/>
         <input 
-          placeholder = "Digite seu nome"
+          placeholder = "Digite a tarefa"
           type="text"
-          value={nome}
-          onChange={(e) => setNome(e.target.value)}
-        /><br/>
-
-        <label>E-mail</label><br/>
-        <input 
-          placeholder = "Digite seu e-mail"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        /><br/>
-
-        <label>Idade</label><br/>
-        <input 
-          placeholder = "Digite sua idade"
-          type="number"
-          value={idade}
-          onChange={(e) => setIdade(e.target.value)}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         /><br/>
 
         <button type="submit">Registrar</button>
@@ -58,11 +38,21 @@ function App() {
 
       <br/><br/>
 
-      {/* Utilizando user.xxxx, os dados só aparecem depois de cadastrados (após onSubmit) */ }
-      {/* Utilizando nome, email e idade os dados aparecem conforme são alterados */}
-      <span>Bem vindo: {user.nome}</span><br/>
-      <span>Email: {user.email}</span><br/>
-      <span>Idade: {user.idade}</span><br/>
+      <ul>
+          {/* Retorno implícito com () */}
+          {tarefas.map((tarefa) => (
+            <li key={tarefa}>{tarefa}</li>
+          ))}
+          
+          {/* Retorno explícito com {} e return
+              Ao abrir o bloco { } é necessário o return
+          */}
+          {/* 
+            {tarefas.map((tarefa) => {
+            return <li key={tarefa}>{tarefa}</li>
+            })} 
+          */}
+      </ul>
     </div>
   );
 }
