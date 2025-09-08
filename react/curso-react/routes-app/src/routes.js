0afb01bd1e0231637185886c47313ab503da39f6
Leonardo Martin <leowork2002@gmail.com>
2025-09-08 12:55:46 -0300

    feat: add contact and header component

--- react/curso-react/routes-app/src/routes.js
+++ react/curso-react/routes-app/src/routes.js
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
+import Contato from './pages/Contato';
+import Header from './components/Header';
 
 function RoutesApp(){
     return(
         <BrowserRouter>
+            <Header />
             <Routes>
                 {/* Ao acessar / sem nada, acessa a homepage */}
                 <Route path="/" element={ <Home/> } />
 
                 <Route path="/sobre" element={ <Sobre/> } />
+
+                <Route path="/contato" element={ <Contato/> } />
             </Routes>
         </BrowserRouter>
     );
