0ea8689ea9bc7d544d7f5f893904805b24a86f8d
Leonardo Martin <leowork2002@gmail.com>
2025-09-08 13:28:50 -0300

    feat: add routes to products and non-existent routes

--- react/curso-react/routes-app/src/routes.js
+++ react/curso-react/routes-app/src/routes.js
@@ -3,6 +3,8 @@ import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
 import Header from './components/Header';
+import Erro from './pages/Erro';
+import Produto from './pages/Produto';
 
 function RoutesApp(){
     return(
@@ -15,6 +17,11 @@ function RoutesApp(){
                 <Route path="/sobre" element={ <Sobre/> } />
 
                 <Route path="/contato" element={ <Contato/> } />
+
+                <Route path="/produto/:id" element={ <Produto/> } />
+
+                {/* Rota para página de erro, caso a rota não exista */}
+                <Route path="*" element={ <Erro/> } />
             </Routes>
         </BrowserRouter>
     );
