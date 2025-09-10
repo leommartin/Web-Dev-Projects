9b097bcca92f43a6f88f28b70a8c01501b4c6ac7
Leonardo Martin <leowork2002@gmail.com>
2025-09-09 23:18:37 -0300

    feat: add project scaffolding, reset css and def routes

---
+++ projetos-cursos/prime-flix/src/App.js
@@ -0,0 +1,14 @@
+// parei na remoção dos arquivos e instalação do react-router-dom
+
+import RoutesApp from './routes';
+
+function App() {
+
+  return (
+    
+    <RoutesApp />
+
+  );
+}
+
+export default App;
