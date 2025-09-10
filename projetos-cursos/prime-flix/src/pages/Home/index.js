1c9fadc720b4f2bc023723b27c320210df3bb29e
Leonardo Martin <leowork2002@gmail.com>
2025-09-10 19:30:57 -0300

    feat: consume movie API with Axios to display now playing movies

--- projetos-cursos/prime-flix/src/pages/Home/index.js
+++ projetos-cursos/prime-flix/src/pages/Home/index.js
@@ -1,9 +1,49 @@
 
+import { useEffect, useState } from 'react';
+import api from '../../services/api';
+import {Link} from 'react-router-dom';
+import './home.css';
+
+
 function Home() {
 
+    const [filmes, setFilmes] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        // https://api.themoviedb.org/3/movie/now_playing?api_key=5c0ea9a2cb10f51220d40a633eecb9ee&language=pt-br
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing" , {
+                params: {
+                    api_key: "5c0ea9a2cb10f51220d40a633eecb9ee",
+                    language: "pt-BR",
+                    page: 1
+                }
+            })
+
+            // console.log(response.data.results.slice(0,10));
+            setFilmes(response.data.results.slice(0,10));
+        }
+
+        loadFilmes();
+    }, [])
+
     return (
-        <div>
-            <h1>Página inicial: Lista de filmes</h1>
+        <div className="container">
+            <div className="lista-filmes">
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original${filme.poster_path}`}
+                                alt={filme.title} 
+                            />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    );
+                })}
+            </div>
         </div>
 
     )
