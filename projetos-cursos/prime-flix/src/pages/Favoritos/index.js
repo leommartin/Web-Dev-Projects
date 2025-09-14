3b6526319c5a75c191ca41a1589ff771a62ebf28
Leonardo Martin <leowork2002@gmail.com>
2025-09-14 00:40:57 -0300

    feat: add page of favorite movies

---
+++ projetos-cursos/prime-flix/src/pages/Favoritos/index.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import './favoritos.css';
+import { Link } from 'react-router-dom';
+function Favoritos() {
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect( () => {
+
+        // recebe a lista em formato de string
+        const minhaLista = localStorage.getItem("@primeflix")
+        setFilmes(JSON.parse(minhaLista) || []);
+
+    }, [])
+
+    return (
+        <div className="meus-filmes">
+            <h1>Filmes favoritos</h1>
+
+            <ul>
+                { filmes.map( (item) => {
+                    return(
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+
+                            <div className="buttons">
+                                <Link to={`/filme/${item.id}`}> Ver detalhes </Link>
+                                <button> Excluir </button>
+                            </div>
+                        </li>
+                    )
+                })}
+
+            </ul>
+
+        </div>
+
+    )
+}
+
+export default Favoritos;
