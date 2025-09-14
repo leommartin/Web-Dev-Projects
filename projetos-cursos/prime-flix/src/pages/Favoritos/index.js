54873ae7ae8e3f50917a48da625fe738d9f09e47
Leonardo Martin <leowork2002@gmail.com>
2025-09-14 01:02:02 -0300

    feat: add action to delete movies

--- projetos-cursos/prime-flix/src/pages/Favoritos/index.js
+++ projetos-cursos/prime-flix/src/pages/Favoritos/index.js
@@ -13,10 +13,21 @@ function Favoritos() {
 
     }, [])
 
+    function excluirFilme(id){
+        alert("ID clicado: " + id);
+
+        let filtroFilmes = filmes.filter( (item) => item.id !== id);
+        // filtro pelo id, todos os ids diferentes permancem na lista
+        
+        setFilmes(filtroFilmes)
+        localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+    }
+
     return (
         <div className="meus-filmes">
             <h1>Filmes favoritos</h1>
 
+            {filmes.length === 0 && <span>Você não tem nenhum filme salvo :( </span> }
             <ul>
                 { filmes.map( (item) => {
                     return(
@@ -25,7 +36,7 @@ function Favoritos() {
 
                             <div className="buttons">
                                 <Link to={`/filme/${item.id}`}> Ver detalhes </Link>
-                                <button> Excluir </button>
+                                <button onClick={ () => excluirFilme(item.id) }> Excluir </button>
                             </div>
                         </li>
                     )
