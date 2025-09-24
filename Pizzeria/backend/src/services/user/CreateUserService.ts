25552c16415eb5402f60700185d08a7287311288
Leonardo Martin <leowork2002@gmail.com>
2025-09-24 19:07:38 -0300

    feat: check [e-mail passed, existence of user by email], create user if not exists

--- Pizzeria/backend/src/services/user/CreateUserService.ts
+++ Pizzeria/backend/src/services/user/CreateUserService.ts
@@ -1,4 +1,4 @@
-
+import prismaClient from '../../prisma';
 
 
 interface UserRequest {
@@ -12,9 +12,37 @@ class CreateUserService {
     // É obrigatório passar todos os dados (nome, email, password) no Controller
     async execute( {name, email, password}: UserRequest ) {
 
-        console.log(name);
+        // Verificar se o email já está cadastrado
 
-        return { name: name };
+        // Verifica se foi passado um email
+        if(!email) {
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        });
+
+        if(userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password, // Precisa ser ocultada na resposta
+            },
+            select: { // Seleciona apenas os campos que quero retornar
+                id: true,
+                name: true,
+                email: true
+            }
+        })
+
+        return user;
     }
 }
 
