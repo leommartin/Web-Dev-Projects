75d758f1e3f0332614889901f0c397f01c272f8d
Leonardo Martin <leowork2002@gmail.com>
2025-09-23 21:04:14 -0300

    feat: install/add prisma and @prisma/client

--- Pizzeria/backend/src/routes.ts
+++ Pizzeria/backend/src/routes.ts
@@ -4,7 +4,14 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({ ok: true });
+    
+    // return res.json({ nome: 'Sujeito Pizza' });
+
+    // imprime 'error: Erro ao fazer essa requisição');
+    // throw new Error('Erro ao fazer essa requisição');
+
+    return res.json({ nome: 'Matheus'});
+
 });
 
 export { router };
--- Pizzeria/backend/src/server.ts
+++ Pizzeria/backend/src/server.ts
@@ -1,14 +1,37 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { router } from './routes';
+import express, { Request, Response, NextFunction } from 'express'
+// import 'express-async-errors';
+import cors from 'cors';
+
+import { router } from './routes'
 
 // Cria uma instância do Express
 const app = express();
 
 // Esse middleware permite que o Express entenda JSON no corpo das requisições
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
- 
+
+// Todas as rotas passam por esse Middleware
+// Trata erros no Express, captura qualquer erro lançado em rotas/middlewares anteriores
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    if(err instanceof Error) {
+        // Se for uma instância do tipo Error
+        return res.status(400).json( {
+            error: err.message // imprime a mensagem que está em routes.ts de error
+        })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+
+    
+});
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
