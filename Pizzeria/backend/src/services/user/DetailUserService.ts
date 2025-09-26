0a52ea9da9bf9b933e41c36c1c92203580e1e4cf
Leonardo Martin <leowork2002@gmail.com>
2025-09-26 20:46:40 -0300

    feat:  search user by req.user_id and return user data

--- Pizzeria/backend/src/services/user/DetailUserService.ts
+++ Pizzeria/backend/src/services/user/DetailUserService.ts
@@ -1,10 +1,24 @@
 import prismaClient from "../../prisma";
 
 class DetailUserService {
-    async execute() {
+    // Controller vai passar o id do usuário para esse service
+    // Esse id vem do token que foi decodificado no middleware isAuthenticated
+    async execute(user_id: string) {
+        
+        // Busca o usuário pelo id no banco de dados
+        const user = await prismaClient.user.findFirst({
+            where: {
+                id: user_id
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            }
+        });
 
-        return {ok: true};
-    
+        // Retorna os dados do usuário
+        return user;
     }
 }
 
