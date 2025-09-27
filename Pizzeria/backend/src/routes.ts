cbdc2104f7f9eff1dc62bfd5ebd5ff5136895a7c
Leonardo Martin <leowork2002@gmail.com>
2025-09-27 13:00:00 -0300

    feat: add route category

--- Pizzeria/backend/src/routes.ts
+++ Pizzeria/backend/src/routes.ts
@@ -6,13 +6,13 @@ import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
 import { isAuthenticated } from './middlewares/isAuthenticated';
+import { CreateCategoryController } from './controllers/category/CreateCategoryController';
 
 const router = Router();
 
+// -- ROTAS DE USUÁRIO
 
-// -- Rotas de Usuários
-
-// -- Rota de criação de usuário 
+// Rota de criação de usuário 
 router.post('/users', new CreateUserController().handle);
 
 router.post('/session', new AuthUserController().handle);
@@ -20,5 +20,10 @@ router.post('/session', new AuthUserController().handle);
 // Rota para pegar os dados do usuário autenticado/logado
 // isAuthenticated -> Middleware que verifica se o usuário está autenticado antes de acessar essa rota
 router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+// -- ROTAS DE CATEGORIA
+
+// Rota para cadastrar uma categoria (apenas para usuários logados)
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router };
