94fc3e7909ffe33d2a718ee952dee7e9452f4fd1
Leonardo Martin <leowork2002@gmail.com>
2025-09-28 12:13:39 -0300

    feat: add route to create and list categories

--- Pizzeria/backend/src/routes.ts
+++ Pizzeria/backend/src/routes.ts
@@ -5,8 +5,11 @@ import { Router } from 'express';
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
 import { DetailUserController } from './controllers/user/DetailUserController';
-import { isAuthenticated } from './middlewares/isAuthenticated';
+
 import { CreateCategoryController } from './controllers/category/CreateCategoryController';
+import { ListCategoryController } from './controllers/category/ListCategoryController'; 
+
+import { isAuthenticated } from './middlewares/isAuthenticated';
 
 const router = Router();
 
@@ -24,6 +27,8 @@ router.get('/me', isAuthenticated, new DetailUserController().handle);
 // -- ROTAS DE CATEGORIA
 
 // Rota para cadastrar uma categoria (apenas para usuários logados)
-router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.post('/category', isAuthenticated, new CreateCategoryController().handle);
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle);
 
 export { router };
