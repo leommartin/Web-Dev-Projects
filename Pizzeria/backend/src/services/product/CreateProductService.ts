dc3944d65bdd0e7932b38e6ef674f621b4febcb9
Leonardo Martin <leowork2002@gmail.com>
2025-10-03 22:08:11 -0300

    feat: create product record in database

--- Pizzeria/backend/src/services/product/CreateProductService.ts
+++ Pizzeria/backend/src/services/product/CreateProductService.ts
@@ -13,7 +13,18 @@ class CreateProductService {
     // Quem chamar o execute deve passar um objeto com os dados do produto
     async execute( { name, price, description, banner, category_id } : ProductRequest ) {
         
-        return {ok : true};
+        const product = await prismaClient.product.create({
+            data: {
+                name: name,
+                price: price,
+                description: description,
+                banner: banner,
+                category_id: category_id
+            }
+        })
+
+        return product;
+
     }
 }
 
