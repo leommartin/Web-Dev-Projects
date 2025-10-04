fb61ab6b7fa3ab10beb1cd81ff50e3d71c25464b
Leonardo Martin <leowork2002@gmail.com>
2025-10-03 22:04:45 -0300

    fix: name of product image

--- Pizzeria/backend/src/config/multer.ts
+++ Pizzeria/backend/src/config/multer.ts
@@ -12,7 +12,7 @@ export default {
                 filename: (req, file, callback) => {
 
                     const fileHash = crypto.randomBytes(16).toString("hex");
-                    const fileName = `${fileHash}-${extname(file.originalname)}`;
+                    const fileName = `${fileHash}-${file.originalname}`;
 
                     return callback(null, fileName);
                 }
