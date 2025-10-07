1400b4a6a0b30d0627d413ba6d7c320246abb283
Leonardo Martin <leowork2002@gmail.com>
2025-10-07 02:18:43 -0300

    feat: add item service/controller and link item to order

---
+++ Pizzeria/backend/src/services/order/AddItemService.ts
@@ -0,0 +1,25 @@
+import prismaClient from "../../prisma";
+
+interface OrderItemRequest {
+    order_id: string;
+    product_id: string;
+    amount: number;
+}
+
+class AddItemService {
+
+    async execute( { order_id, product_id, amount } : OrderItemRequest ) {
+        
+        const orderItem = await prismaClient.item.create({
+            data: {
+                order_id: order_id,
+                product_id: product_id,
+                amount: amount
+            }
+        })
+
+        return orderItem;
+    }
+}
+
+export { AddItemService };
