e4df87a2552d145eabdd10e93c9f22c21ba01e76
Leonardo Martin <leowork2002@gmail.com>
2025-10-08 16:41:50 -0300

    feat: add route to finish an order

--- Pizzeria/backend/src/routes.ts
+++ Pizzeria/backend/src/routes.ts
@@ -17,10 +17,11 @@ import { CreateOrderController } from './controllers/order/CreateOrderController
 import { RemoveOrderController } from './controllers/order/RemoveOrderController';
 import { ListOrderController } from './controllers/order/ListOrderController';
 import { DetailOrderController } from './controllers/order/DetailOrderController';
+import { FinishOrderController } from './controllers/order/FinishOrderController';
+import { SendOrderController } from './controllers/order/SendOrderController';
 
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
-import { SendOrderController } from './controllers/order/SendOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -67,5 +68,7 @@ router.put('/order/send', isAuthenticated, new SendOrderController().handle);
 
 router.get('/orders', isAuthenticated, new ListOrderController().handle);
 router.get('/order/detail', isAuthenticated, new DetailOrderController().handle);
+
+router.put('/order/finish', isAuthenticated, new FinishOrderController().handle);
 
 export { router };
