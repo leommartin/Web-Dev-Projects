351a95b43f864ddad41d4b18f4e19801e357a891
Leonardo Martin <leowork2002@gmail.com>
2025-10-21 00:03:21 -0300

    feat: add sign-up page

--- Pizzeria/frontend/src/app/page.tsx
+++ Pizzeria/frontend/src/app/page.tsx
@@ -1,10 +1,49 @@
-import Image from "next/image";
 import styles from "./page.module.scss";
+// import logoImg from '/public/logo.svg';
+import Image from 'next/image';
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <main>
-        <h1 className={styles.teste}>TESTE TESTE</h1>
-    </main>
+    <>
+      <div className={styles.containerCenter}>
+        {/* public assets are served from the project root — don't include '/public' in the path */}
+        <Image
+          src="/logo.svg"
+          alt="Logo Sujeito Pizzaria"
+          width={160}
+          height={160}
+        />
+
+        <section className={styles.login}>
+          <form>
+            <input 
+              type = "email"
+              required name="email" 
+              placeholder="Digite seu email..." 
+              className={styles.input} 
+            />
+
+            <input 
+              type = "password" 
+              required name="password" 
+              placeholder="********" 
+              className={styles.input} 
+            />
+
+            <button type="submit" className={styles.button}> 
+              Acessar 
+            </button>
+
+            <Link href="/signup" className={styles.text}>
+              Não possui uma conta? Cadastre-se
+            </Link>
+
+            
+
+          </form>
+        </section>
+      </div>
+    </>
   );
 }
